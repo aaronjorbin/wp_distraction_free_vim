f9bc29e9580472256fc59c0aa08814e10799e06e
Aaron Jorbin <aaron@jorb.in>
2011-11-04 23:43:06 -0400

    random bug fixes

--- dfv.js
+++ dfv.js
@@ -1,11 +1,15 @@
+var dfv;
+
 (function($){
-    var visualMode = false,
+    var api,
+        visualMode = false,
         commands = {
             ':w' : function(){ fullscreen.save(); },
             ':q': function(){ fullscreen.off(); },
             'i': function(){ enterInputMode();  }
         };
 
+	dfv = api = {};
    
     // cross browser listeners
     function addEvent(object, event, method) {
@@ -51,33 +55,36 @@
         key = event.keyCode;
         if ( fullscreen.settings.visible && key == 27) 
             enterVisualMode();
+
     }
 
     function listenForCommands(event){
         key = event.keyCode;
         if (  fullscreen.settings.visible && key == 13)
+        {
             processCommand();
+        }
+        
     }
-
     function processCommand(){
         var cb;
-        if ( commands.hasOwnProperty( $('#dfvInput').val() ) ){
-             cb = commands[ $('#dfvInput').val() ];
-             cb();
+        if ( commands.hasOwnProperty( $('#dfvInput').val() ) ){ 
+            cb = commands[ $('#dfvInput').val() ];
+            cb();
+            $('#dfvInput').val('');
         }
     }
 
-    // Instead of closing DFW, escape should show the menubar and enter visual mode
+    // Instead of closing DFW, escape should show the menubar when in visual mode
     jQuery(document).bind( 'wp_CloseOnEscape', function(e, data){ 
         data.cb = function(e) { 
             fullscreen.bounder( 'showToolbar', 'hideToolbar', 10000, e );  
             if (visualMode == false)
                 enterVisualMode();  
-            else
-                $('#dfvInput').focus();
         };   
-    });
+    } );
 
-    jQuery('#wp_mce_fullscreen').focus('enterInputMode');
-
+//    jQuery(document).bind( '
+    jQuery('#wp_mce_fullscreen').bind('focus', 'enterInputMode');
 })(jQuery);
+
